16f2744f50149c3c3bea70c0983ede300e51fd07
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-19 13:42:39 -0300

    Presgined url generation function

---
+++ thumbnail-generator-api/src/functions/upload-url-generator.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { S3 } from "aws-sdk";
+import { badRequest, ok } from "../utils/responses";
+
+type UploadEventBody = {
+  fileName: string;
+};
+
+const s3Client = new S3();
+const uploadPath = 'uploads/';
+
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const bucket = process.env.BUCKET_NAME;
+  if (!bucket) {
+    throw new Error("Bucket name is not defined");
+  }
+
+  let fileName;
+  try {
+    const input = JSON.parse(event.body ?? '') as UploadEventBody;
+    fileName = input.fileName;
+  } catch (err) {
+    return badRequest("Invalid request body")
+  }
+
+  if (!fileName) {
+    return badRequest("File name is not defined");
+  }
+
+  const fullPath = uploadPath + fileName;
+  const params = {
+    Bucket: bucket,
+    Key: fullPath,
+    Expires: 1200, // 20 minutes
+  };
+
+  const upload_url = await s3Client.getSignedUrlPromise('putObject', params);
+
+  return ok({ upload_url });
+};
