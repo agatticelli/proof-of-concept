0c37006d2b10d258fc28b5227ae44e2bb8458b93
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-19 13:34:43 -0300

    Add lambda function to generate presignedUrl

--- thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
+++ thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
@@ -1,12 +1,34 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+
 export class ThumbnailGeneratorApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'ThumbnailBucket', {
+    const bucket = new Bucket(this, 'ThumbnailBucket', {
       bucketName: 'thumbnails-store',
     });
+
+    // create lambda function to update images to s3
+    const uploadHandler = new NodejsFunction(this, 'ImageUploadHandler', {
+      entry: 'src/functions/uploader.ts',
+      handler: 'handle',
+      runtime: Runtime.NODEJS_14_X,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      }
+    });
+
+    // grant lambda function access to s3
+    bucket.grantPut(uploadHandler);
+
+    // create API gateway to expose upload handler
+    const api = new RestApi(this, 'ThumbnailGeneratorApi');
+    const uploadEndpoint = api.root.addResource('images');
+    uploadEndpoint.addMethod('POST', new LambdaIntegration(uploadHandler));
   }
 }
