50553477b72d3edc42087e0d898e175f00fc9d59
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-20 12:03:40 -0300

    Add uploader code

--- thumbnail-generator-api/src/functions/uploader.ts
+++ thumbnail-generator-api/src/functions/uploader.ts
@@ -1,3 +1,70 @@
-export const handler = (event) => {
-  
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { fileTypeFromBuffer } from 'file-type';
+
+
+
+import { badRequest, internalServerError, ok } from "../utils/responses";
+
+type UploadEventBody = {
+  fileName: string;
+  content: string;
+};
+
+const client = new S3Client({});
+const uploadPath = 'uploads/';
+const validMimes = ['image/jpeg', 'image/png'];
+
+export const handle = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  const bucket = process.env.BUCKET_NAME;
+  if (!bucket) {
+    throw new Error("Bucket name is not defined");
+  }
+
+  let fileName;
+  let fileContent;
+  try {
+    const input = JSON.parse(event.body ?? '') as UploadEventBody;
+    fileName = input.fileName;
+    fileContent = input.content;
+  } catch (err) {
+    return badRequest("Invalid request body");
+  }
+
+  if (!fileName) {
+    return badRequest("File name is not defined");
+  }
+
+  if (!fileContent) {
+    return badRequest("File content is not defined");
+  }
+
+  // create buffer from file content
+  const fileBuffer = Buffer.from(fileContent, 'base64');
+
+  // get real file type
+  const filetype = await fileTypeFromBuffer(fileBuffer);
+  if (!filetype) {
+    return badRequest("Invalid file");
+  }
+
+  if (!validMimes.includes(filetype.mime)) {
+    return badRequest("File type is not supported");
+  }
+
+  // check maxlength of 5mb
+  if (fileBuffer.length > 5 * 1024 * 1024) {
+    return badRequest("File is too large. Max: 5mb");
+  }
+
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: `${uploadPath}${fileName}`,
+    Body: fileBuffer,
+  });
+  const response = await client.send(command);
+
+  console.log(response);
+
+  return ok({})
 }
