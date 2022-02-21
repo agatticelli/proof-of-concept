c3f433404f0c6b83f327ff51b588b8f79c630813
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-21 11:06:19 -0300

    Add redirect to responses

--- thumbnail-generator-api/src/utils/responses.ts
+++ thumbnail-generator-api/src/utils/responses.ts
@@ -1,6 +1,7 @@
-const responseBuilder = (statusCode: number, data: any) => ({
+const responseBuilder = (statusCode: number, data: any, headers?: any) => ({
   statusCode,
-  body: JSON.stringify(data),
+  body: data ? JSON.stringify(data) : '',
+  headers,
 });
 const error = (statusCode: number, message: string) => responseBuilder(statusCode, { message });
 
@@ -8,6 +9,7 @@ export const ok = (data: any) => responseBuilder(200, data);
 export const created = (data: any) => responseBuilder(201, data);
 export const noContent = () => responseBuilder(204, {});
 
+export const redirect = (url: string, code = 301) => responseBuilder(code, null, { Location: url });
 export const badRequest = (message = 'Bad request') => error(400, message);
 export const notFound = (message = 'Not found') => error(404, message);
 export const internalServerError = (message = 'Internal server error') => error(500, message);
