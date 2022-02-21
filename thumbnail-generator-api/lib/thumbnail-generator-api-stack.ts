4536beea7229e8df4908b669de6cd952b9814dcf
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-21 10:33:09 -0300

    Add definition for thumbnail generator function

--- thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
+++ thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
@@ -2,13 +2,15 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Construct } from 'constructs';
 
 export class ThumbnailGeneratorApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // --------- IMAGE CREATION ---------
     // create bucket to store uploaded images
     const bucket = new Bucket(this, 'ThumbnailBucket', {
       bucketName: 'thumbnails-store',
@@ -29,7 +31,28 @@ export class ThumbnailGeneratorApiStack extends Stack {
 
     // create API gateway to expose upload handler
     const api = new RestApi(this, 'ThumbnailGeneratorApi');
-    const uploadEndpoint = api.root.addResource('images');
-    uploadEndpoint.addMethod('POST', new LambdaIntegration(uploadHandler));
+    const imageEndpoint = api.root.addResource('images');
+    imageEndpoint.addMethod('POST', new LambdaIntegration(uploadHandler));
+
+
+    // --------- THUMBNAIL GENERATION ---------
+    // create resize handler
+    const resizerHandler = new NodejsFunction(this, 'ImageResizerHandler', {
+      entry: 'src/functions/resizer.ts',
+      handler: 'handle',
+      runtime: Runtime.NODEJS_14_X,
+      bundling: {
+        nodeModules: ['sharp'],
+      }
+    });
+
+    // bind s3 event source to resize handler
+    resizerHandler.addEventSource(new S3EventSource(bucket, {
+      events: [EventType.OBJECT_CREATED],
+      filters: [{ prefix: 'uploads/' }],
+    }));
+
+    // grant resize function to read/write from/to bucket
+    bucket.grantReadWrite(resizerHandler);
   }
 }
