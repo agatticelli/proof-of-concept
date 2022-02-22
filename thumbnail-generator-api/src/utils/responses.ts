c751dc6370da6b00d3af0becbf51b0045cd18d35
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-22 02:31:12 -0300

    Remove unused responses

--- thumbnail-generator-api/src/utils/responses.ts
+++ thumbnail-generator-api/src/utils/responses.ts
@@ -6,10 +6,6 @@ const responseBuilder = (statusCode: number, data: any, headers?: any) => ({
 const error = (statusCode: number, message: string) => responseBuilder(statusCode, { message });
 
 export const ok = (data: any) => responseBuilder(200, data);
-export const created = (data: any) => responseBuilder(201, data);
-export const noContent = () => responseBuilder(204, {});
-
 export const redirect = (url: string, code = 301) => responseBuilder(code, null, { Location: url });
-export const badRequest = (message = 'Bad request') => error(400, message);
-export const notFound = (message = 'Not found') => error(404, message);
-export const internalServerError = (message = 'Internal server error') => error(500, message);
+export const badRequest = (message = 'Bad Request') => error(400, message);
+export const notFound = (message = 'Not Found') => error(404, message);
