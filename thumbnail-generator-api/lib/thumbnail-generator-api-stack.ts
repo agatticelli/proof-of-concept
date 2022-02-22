6977ed79370dde76309d21f9204d5571194b69eb
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-21 21:27:06 -0300

    Add cognito auth

--- thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
+++ thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
@@ -1,9 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import { RestApi, LambdaIntegration, CognitoUserPoolsAuthorizer } from 'aws-cdk-lib/aws-apigateway';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
 import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { UserPool, AccountRecovery, UserPoolClient, UserPoolClientIdentityProvider } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 import { bucketName, thumbnailsSizes } from '../src/config';
@@ -12,10 +13,49 @@ export class ThumbnailGeneratorApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // --------- COGNITO AUTH ---------
+    // create cognito user pool
+    const userPool = new UserPool(this, 'UserPool', {
+      userPoolName: `my-user-pool`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: false,
+        requireDigits: false,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+      accountRecovery: AccountRecovery.EMAIL_ONLY,
+    });
+
+    // create the user pool client
+    const userPoolClient = new UserPoolClient(this, 'UserPoolClient', {
+      userPool,
+      authFlows: {
+        adminUserPassword: true,
+        userPassword: true,
+        custom: true,
+      },
+      supportedIdentityProviders: [
+        UserPoolClientIdentityProvider.COGNITO,
+      ],
+    });
+
+    // create the authorizer
+    const authorizer = new CognitoUserPoolsAuthorizer(this, 'Authorizer', {
+      cognitoUserPools: [userPool],
+      authorizerName: 'ThumbnailGeneratorApiAuthorizer',
+      identitySource: 'method.request.header.Authorization',
+    });
+
     // --------- IMAGE CREATION ---------
     // create bucket to store uploaded images
     const bucket = new Bucket(this, 'ThumbnailBucket', {
       bucketName,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
     // create lambda function to update images to s3
@@ -35,7 +75,9 @@ export class ThumbnailGeneratorApiStack extends Stack {
     // create API gateway to expose upload handler
     const api = new RestApi(this, 'ThumbnailGeneratorApi');
     const imageEndpoint = api.root.addResource('images');
-    imageEndpoint.addMethod('POST', new LambdaIntegration(uploadHandler));
+    imageEndpoint.addMethod('POST', new LambdaIntegration(uploadHandler), {
+      authorizer, // authorize uploads
+    });
 
 
     // --------- THUMBNAIL GENERATION ---------
@@ -78,5 +120,10 @@ export class ThumbnailGeneratorApiStack extends Stack {
 
     // grant downloader function to read from bucket
     bucket.grantRead(downloadThumbnailHandler);
+
+
+    // --------- OUTPUT ---------
+    new CfnOutput(this, 'userPoolId', { value: userPool.userPoolId });
+    new CfnOutput(this, 'userPoolClientId', { value: userPoolClient.userPoolClientId });
   }
 }
